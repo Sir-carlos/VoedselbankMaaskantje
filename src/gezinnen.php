02380f701d945a69672c5ed60ad8426a3f7a7bd1
Tommy <tommykinfu@gmail.com>
2024-03-05 10:59:39 +0100

    converting js to php

--- src/gezinnen.php
+++ src/gezinnen.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link href="src\style.css" rel="styleseheet">
     <title>Gezinnen Pagina</title>
 </head>
 <body>
@@ -18,6 +18,23 @@
         <a href="login.html" style="float: right;">Login</a>
     </div>
     <h1>Gezinnen</h1>
+
+    <table class="gdata">
+        <tr>
+            <th>Gezinnen Naam</th>
+            <th>Adres</th>
+            <th>Telefoonnummer</th>
+            <th>Email</th>
+            <th>Aantal</th>
+        </tr>
+        <tr>
+            <td>Gezinnen Naam</td>
+            <td>Adres</td>
+            <td>Telefoonnummer</td>
+            <td>Email</td>
+            <td>Aantal</td>
+        </tr>
+    </table>
     
 </body>
     <script src="script.js"></script>
