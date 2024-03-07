ce9ece0d5f70692b6405b95018cd2f0611fc2138
JayGupGup <71427304+JayGupGup@users.noreply.github.com>
2024-03-07 16:05:05 +0100

    added button to home

--- src/index.php
+++ src/index.php
@@ -10,6 +10,13 @@
 <body>
     <h1>Home</h1>
     <p>bla bla bla bla bla</p>
+    <div class="button-container">
+
+
+
+<button class="button-85" role="button">Button 85</button>
+
+</div>
     
 </body>
     <script src="script.js"></script>
