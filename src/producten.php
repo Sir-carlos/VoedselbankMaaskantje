39409ff3bc949e6cf0872b3ecaf59b1127c72bcf
Alec <aarnaoutoglou@roc-dev.com>
2024-03-11 15:59:35 +0100

    added styling for tables
    removed table code in css file

--- src/producten.php
+++ src/producten.php
@@ -7,11 +7,45 @@
     <link rel="stylesheet" href="style.css">
     <title>Producten</title>
     <?php require_once 'database.php';?>
+
+    <style>
+        .content-table {
+            border-collapse: collapse;
+            margin: 25px 0;
+            font-size: 0.9em;
+            min-width: 400px;
+            border-radius: 5px 5px 0 0;
+            overflow: hidden;
+            box-shadow: 0 0 1px grey;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 100px;
+            width: 80%;
+        }
+
+        .content-table thead {
+            background-color: #00BF63;
+            color: #ffffff;
+            text-align: left;
+            font-weight: bold;
+        }
+        
+        .content-table th,
+        .content-table td {
+            padding: 12px 15px;
+        }
+
+        .content-table tbody tr {
+            border-bottom: 1px solid #dddddd;
+        }
+
+    </style>
 </head>
 <body>
     <h1>Producten</h1>
     
-    <table>
+<table class="content-table">
+    <thead>
         <tr>
             <th>Product Naam</th>
             <th>Categorie</th>
@@ -19,6 +53,8 @@
             <th>Aantal</th>
             <th>Acties</th>
         </tr>
+    </thead>
+    <tbody>
         <?php
         $query = $dbh->prepare(
             "SELECT * FROM producten;");
@@ -36,7 +72,8 @@
                   );
             }
         ?>
-    </table>
+    </tbody>
+</table>
 
 </body>
     <script src="script.js"></script>
