af9f6b0d31b8ae15a63f420cac33a085a853fee5
Alec <aarnaoutoglou@roc-dev.com>
2024-03-19 11:38:41 +0100

    Revert "made loginsystem folder"
    This reverts commit 83fbc1cc0247101dcc1dcc1aa4dcd4dbcc359dc4.

--- src/loginsystem/wachtwoord.php
+++ src/loginsystem/wachtwoord.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="loginsystem/wachtwoord_check.php" method="POST">
+    <form action="wachtwoord_check.php" method="POST">
         <h1>Verander wachtwoord</h1>
 
         <br>
