71b3def817006e713b1a25b2736b29d5f4516c34
Tommy545 <39779570+Tommy545@users.noreply.github.com>
2024-03-25 11:36:46 +0100

    Update producten.php

--- src/producten.php
+++ src/producten.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'header.php' ?>
+<?php include 'header.php'; require_once 'database.php'; ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -41,7 +41,7 @@
                     <td>" . $value["aantal"] . '</td>
                     <td> <a href="producten_bewerken.php"><img src="bewerken.png" class="bimg"></a> </td>
                     </tr>'
-                  );
+                  );    
             }
         ?>
     </table>
